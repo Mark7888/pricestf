efea67f5a8ecf3800bbc49a136c8439bfc78332f
Mark7888 <l.mark7888@gmail.com>
2021-01-12 01:26:26 +0100

    added history entrypoint

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     name='pricestf',
     license="MIT",
     description='python api to Nicklason\'s prices.tf site.',
-    version='v1.1.0',
+    version='v1.1.1',
     long_description_content_type='text/markdown',
     long_description=README,
     url='https://github.com/Mark7888/pricestf',
